d0a701112ed6d6be1d3c7951647d93e4ae85a03c
miinaw <yong.abema@gmail.com>
2019-04-02 00:39:49 +0900

    add:vue => about page

---
+++ src/pages/About.vue
@@ -0,0 +1,19 @@
+<template>
+ <div class="page about">
+   <Title title="About me."/>
+ </div>
+</template>
+
+<script>
+import Title from '../components/Title.vue'
+
+export default {
+  name: 'about',
+  components: {
+    Title,
+  },
+}
+</script>
+
+<style>
+</style>
