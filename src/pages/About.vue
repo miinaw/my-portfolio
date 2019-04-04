b63016ee0b7b600e3998695127d8028fe9b4e543
miinaw <yong.abema@gmail.com>
2019-04-05 00:33:52 +0900

    wip:About.vue

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,7 +1,19 @@
 <template>
- <div class="page about">
-   <Title title="About me."/>
- </div>
+  <div class="page about">
+    <a href="/">
+      <img class="logo" alt="logo" src="../assets/images/logo.jpg">
+    </a>
+    <Title title="About me." description="Hi."/>
+    <div class="page-content">
+      <div class="heading">Profile.</div>
+        <p>横山美波 Minami Yokoyama</p>
+        <p>1996.01.06</p>
+        <p>宮崎大学工学部 情報システム工学科 2018卒</p>
+        <p>好きなたべものはチョコレート</p>
+      <div class="heading">Life so far.</div>
+      <div class="heading">The me today.</div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -15,5 +27,12 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+.heading {
+  font-size: 1.8rem;
+  font-weight: bold;
+}
+p + .heading {
+  margin-top: 2rem;  
+}
 </style>
