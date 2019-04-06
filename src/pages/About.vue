6993c55f926fff681b4e588d580818bd896572b4
miinaw <yong.abema@gmail.com>
2019-04-06 19:02:54 +0900

    wip:About.vue => 内容記述

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -8,14 +8,29 @@
       <div class="heading">
         <h1 class="heading-title">Profile.</h1>
       </div>
-        <p>横山美波 Minami Yokoyama</p>
-        <p>1996.01.06</p>
-        <p>宮崎大学工学部 情報システム工学科 2018卒</p>
-        <p>好きなたべものはチョコレート</p>
+      <div class="body">
+        <p>横山美波(Minami Yokoyama)</p>
+        <p>1996年1月6日生まれ</p>
+        <p>宮崎県出身</p>
+        <p>宮崎大学工学部 情報システム工学科 2018年卒</p>
+        <p>株式会社アラタナに新卒入社(フロントエンドエンジニア)</p>
+        <p>好きなたべものはチョコレート(よく食べるのはブラックサンダーゴールドとチロルチョコのミルク)</p>
+        <p>好きな動物は猫</p>
+      </div>
+      
       <div class="heading">
         <h1 class="heading-title">Life so far.</h1>
       </div>
-      <p></p>
+      <div class="body">
+        <p>小さい頃からも絵を描くことやものづくりが好きだったためデザイン専門の高校を受けようとしたが、兄の影響で情報システムの道に進む。</p>
+        <p>高校、大学ではC・mySQL・PHP・Rなどを触る。</p>
+        <p>高校の卒業研究ではkinectを用いた「仮想試着室」を実装。</p>
+        <p>大学の卒業研究では筋電位や脳波を用いた生体認証の研究を行う。</p>
+        <p>大学時代は経験のため、株式会社アダストリアのブランドでの販売や、地元テレビ局でFDをアルバイトとして3年ほど務める。</p>
+        <p>IT業界やアパレル業界に興味をもっていたこともあり、株式会社アラタナに入社し、フロントエンドエンジニアに配属される。</p>
+        <p>主にZOZOTOWN取扱いブランドの自社ECサイトのフロント部分の実装を行う。</p>
+      </div>
+      
       <div class="heading">
         <h1 class="heading-title">The me today.</h1>
         <p>My instagram photos.</p>
@@ -69,6 +84,20 @@ export default {
   margin-bottom: .5rem;
   font-size: 1.8rem;
   font-weight: bold;
+}
+.body {
+  margin: 0 2rem;
+  p {
+    text-align: left;
+    font-size: .9rem;
+    line-height: 1.4em;
+  }
+  p + p {
+    margin-top: .5rem;
+  }
+  + .heading {
+    margin-top: 2rem;
+  }
 }
 p + .heading {
   margin-top: 2rem;  
