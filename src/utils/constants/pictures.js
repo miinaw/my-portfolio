58cdcd5d58e40a3dfe9c3d128ad88a80d2a146bb
miinaw <yong.abema@gmail.com>
2019-05-17 02:21:09 +0900

    add: images (temporary)

---
+++ src/utils/constants/pictures.js
@@ -0,0 +1,47 @@
+export const PICTURE_LIST = [
+  {
+    id: 0,
+    name: "kekkan",
+    url: "/pictures/kekkan.jpg"
+  },
+  {
+    id: 1,
+    name: "boy&girl",
+    url: "/pictures/boy&girl.jpg"
+  },
+  {
+    id: 2,
+    name: "my brother",
+    url: "/pictures/my_bro.jpg"
+  },
+  {
+    id: 3,
+    name: "me",
+    url: "/pictures/me.jpg"
+  },
+  {
+    id: 4,
+    name: "Kou Mabuchi",
+    url: "/pictures/kou_mabuchi.jpg"
+  },
+  {
+    id: 5,
+    name: "my friend",
+    url: "/pictures/my_friend.jpg"
+  },
+  {
+    id: 6,
+    name: "eye",
+    url: "/pictures/3E.jpg"
+  },
+  {
+    id: 7,
+    name: "eye",
+    url: "/pictures/dammy.png"
+  },
+  {
+    id: 8,
+    name: "eye",
+    url: "/pictures/dammy.png"
+  },
+];
